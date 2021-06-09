e489dc257f8b5db1fc7d1c0d0cf10d78573d4790
mattdavis0351 <mattdavis0351@github.com>
2021-06-09 11:50:27 -0700

    add action files

--- .github/actions/gitignore-parser-action/lib/gradeLearner.js
+++ .github/actions/gitignore-parser-action/lib/gradeLearner.js
@@ -2,6 +2,61 @@ const fs = require("fs");
 module.exports = () => {
   const gitingnore = `${process.env.GITHUB_WORKSPACE}/.gitignore`;
 
-  const contents = fs.readFileSync(gitingnore, "utf8").split();
-  console.log(contents);
+  const answers = ["z*", ".env", "artifacts/"];
+  const contents = fs.readFileSync(gitingnore, "utf8").split("\n");
+  try {
+    const results = answers.filter((i) => {
+      if (!contents.includes(i)) return i;
+    });
+    console.log(results);
+    if (results.length === 0) {
+      return {
+        reports: [
+          {
+            filename: ".gitignore",
+            isCorrect: true,
+            display_type: "actions",
+            level: "info",
+            msg: "Great job!  You have sucessfully configured the .gitignore file for this repository",
+            error: {
+              expected: "",
+              got: "",
+            },
+          },
+        ],
+      };
+    } else {
+      return {
+        reports: [
+          {
+            filename: ".gitignore",
+            isCorrect: false,
+            display_type: "actions",
+            level: "warning",
+            msg: "",
+            error: {
+              expected: ".env, artifacts/, z* to exist in the .gitignore file",
+              got: `${results.join()}`,
+            },
+          },
+        ],
+      };
+    }
+  } catch (error) {
+    return {
+      reports: [
+        {
+          filename: ".gitignore",
+          isCorrect: false,
+          display_type: "actions",
+          level: "fatal",
+          msg: "",
+          error: {
+            expected: "",
+            got: "An internal error occured.  Please open an issue at: https://github.com/githubtraining/exercise-use-gitignore and let us know!  Thank you",
+          },
+        },
+      ],
+    };
+  }
 };
